25baec596823f1dcfbd40fc28aa7e05a19a5ec5b
MAGANER <maganer.krik@mail.ru>
2022-02-25 19:32:55 +0300

    now parser is able to parse line into sequence of Nodes of 2 types:tag and text

--- Parser.cpp
+++ Parser.cpp
@@ -2,6 +2,21 @@
 using namespace Parser;
 
 
+std::string Parser::prepare(const std::string& line)
+{
+	
+	using namespace std;
+	regex reg_right(">");
+	regex reg_left("<");
+
+	string updated = line;
+	if (regex_search(updated.c_str(), reg_right))
+		updated = regex_replace(updated, reg_right, "> ");
+	if (regex_search(updated.c_str(), reg_left))
+		updated = regex_replace(updated, reg_left, " <");
+
+	return updated;
+}
 tags_str Parser::get_tag(const std::string& line)
 {
 	using namespace std;
@@ -13,7 +28,7 @@ tags_str Parser::get_tag(const std::string& line)
 	function<void(const string&, vector<string>&)> _gt = [&](const string& _line,
 		vector<string>& result)
 	{
-		auto line = put_new_line_ch(_line);
+		auto line = put_new_line_ch(prepare(_line));
 		cmatch m;
 		regex_search(line.c_str(), m, reg);
 		if (m.empty())
@@ -24,16 +39,89 @@ tags_str Parser::get_tag(const std::string& line)
 		else
 		{
 			result.push_back(*m.begin());
-			auto updated = regex_replace(line, regex((*m.begin()).str()), "");
+			auto updated = regex_replace(line, regex((*m.begin()).str()), DELIM);
 			_gt(updated, result);
 		}
 	};
 	_gt(line, result);
 
-	return make_pair(result,text);
+	return make_pair(result,change_spaces(text));
 }
 std::string Parser::put_new_line_ch(const std::string& line)
 {
 	auto reg = std::regex("<br>");
 	return std::regex_replace(line.c_str(), reg, "\n");
+}
+std::vector<std::string> Parser::split(std::string s, std::string delimiter)
+{
+	using namespace std;
+	size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+	string token;
+	vector<string> res;
+
+	while ((pos_end = s.find(delimiter, pos_start)) != string::npos) 
+	{
+		token = s.substr(pos_start, pos_end - pos_start);
+		pos_start = pos_end + delim_len;
+		res.push_back(token);
+	}
+
+	res.push_back(s.substr(pos_start));
+	return res;
+}
+std::vector<Node*> Parser::parse(const std::string& line)
+{
+	using namespace std;
+	auto tag = get_tag(line);
+	auto splitted_text = split(return_spaces(tag.second), " ");
+
+	vector<Node*> result;
+	int tag_counter = 0;
+	for (auto token : splitted_text)
+	{
+		auto _ = regex_replace(token.c_str(), regex("_"), " ");
+		if (!is_spaceful(_))
+		{
+			bool is_tag = regex_search(_.c_str(), regex(".*[#$#].*"));
+			if (is_tag)
+			{
+				result.push_back(make_tag(tag.first[tag_counter]));
+				tag_counter++;
+			}
+			else
+			{
+				result.push_back(make_text(_));
+			}
+		}
+	}
+
+	return result;
+}
+bool Parser::is_spaceful(const std::string& line)
+{
+	using namespace std;
+	regex space("[ \t\r\n\f]{"+to_string(line.size())+"}");
+	if (regex_match(line.c_str(), space)) return true;
+	else return false;
+}
+std::string Parser::change_spaces(const std::string& line)
+{
+	using namespace std;
+	string result;
+	for (auto& n : line)
+	{
+		if (isspace(n))result += "_";
+		else result += n;
+	}
+	return result;
+}
+std::string Parser::return_spaces(const std::string& line)
+{
+	using namespace std;
+
+	regex reg_left("_{2,}");
+	regex reg_right("_{2,}");
+
+	auto updated = regex_replace(line.c_str(), reg_left, "_ ");
+	return regex_replace(updated.c_str(), reg_right, " _");
 }
--- Parser.h
+++ Parser.h
@@ -5,11 +5,51 @@
 #include<regex>
 #include<functional>
 #include<utility>
+#include<iostream>
 namespace Parser
 {
+	static const std::string DELIM = "#$#";
 	typedef std::pair<std::vector<std::string>, std::string> tags_str;
 
+	struct Node
+	{
+		enum class NodeType { tag, text };
+		NodeType type;
+		std::string val;
+	};
+	struct Tag: public Node
+	{
+		Tag(const std::string& val)
+		{
+			this->val = val;
+			type = NodeType::tag;
+		}
+		~Tag(){}
+	};
+	struct Text : public Node
+	{
+		Text(const std::string& val)
+		{
+			this->val = val;
+			type = NodeType::text;
+		}
+	};
+	static Node* make_text(const std::string& val)
+	{ 
+		return  new Text(val);
+	}
+	static Node* make_tag(const std::string& val)
+	{
+		return new Tag(val);
+	}
+
+	std::string change_spaces(const std::string& line);
+	std::string return_spaces(const std::string& line);
+	std::vector<Node*> parse(const std::string& line);
+	std::vector<std::string> split(std::string s, std::string delimiter);
 	std::string put_new_line_ch(const std::string& line);
+	std::string prepare(const std::string& line);
 	tags_str get_tag(const std::string& line);
+	bool is_spaceful(const std::string& line);
 };
 #endif //PARSER_H
